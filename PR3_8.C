7df956c87fdd0d5b3c25c72550ee629dad694f16
imparasparabiya <137284876+imparasparabiya@users.noreply.github.com>
2023-07-07 16:20:16 +0530

    Add files via upload

---
+++ PR3_8.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+  int i,n,count=0;
+  clrscr();
+  printf("Enter a Number = ");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+   {
+       if(n%i==0)
+      {
+	count++;
+      }
+   }
+     if(count==2)
+     {
+      printf("%d is a Prime Number\n",n);
+     }
+     else
+     {
+      printf("%d is not a Prime Number\n",n);
+     }
+
+
+  getch();
+}
