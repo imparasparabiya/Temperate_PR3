29f9ce44bef8633351d627bb321cf7e910bcb0d8
imparasparabiya <137284876+imparasparabiya@users.noreply.github.com>
2023-07-07 14:08:27 +0530

    Add files via upload

---
+++ PR3_5.C
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+ int n,t,p,r=0;
+ clrscr();
+ printf("Enter a Number = ");
+ scanf("%d",&n);
+ t=n;
+ while(n>0)
+   {
+    p=n%10;
+    r=r*10+p;
+    n/=10;
+
+   }
+    printf("Reverse a Number = %d\n",r);
+
+   if(t==r)
+      {
+       printf("Your Number is Palindrom\n");
+      }
+      else
+      {
+       printf("Your Number is not Palindrom\n");
+      }
+
+ getch();
+
+}
